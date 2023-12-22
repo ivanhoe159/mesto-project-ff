7e04908bc68c45a8cbabc8b892febd6cb7e4a066
Anton <87985384+ivanhoe159@users.noreply.github.com>
2023-12-22 22:21:25 +0700

    Update cards.js

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-  {
-    name: 'Фиджи',
-    link: 'https://i.imgur.com/8tu458O.png'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://i.imgur.com/s4PVMfx.png'
-  },
-  {
-    name: 'Домбай',
-    link: 'https://i.imgur.com/PcDX4xN.png'
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: 'https://i.imgur.com/h7lkQGa.png'
-  },
-  {
-    name: 'Обь',
-    link: 'https://i.imgur.com/KSj7Mfu.png'
-  },
-  {
-    name: 'Эйяфьядлайёкюдль',
-    link: 'https://i.imgur.com/2KOIoIy.png'
-  }
-]; 
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    }
+];
