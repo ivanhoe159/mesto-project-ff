17b504a9660f1f6fdea2af83fd18ac84fddb521f
Anton <87985384+ivanhoe159@users.noreply.github.com>
2023-12-22 09:55:27 +0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const page = document.querySelector('.page');
+const placesList = page.querySelector('.places__list');
+const cardTemplate = page.querySelector('#card-template');
 
-// @todo: DOM узлы
+const deleteCard = function() {
+    const currentCard = this.closest('.card');
+    currentCard.remove();
+}
 
-// @todo: Функция создания карточки
+function addCard(initialCard, deleteCard) {
+    const newCard = cardTemplate.content.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__image').src = initialCard.link;
+    newCard.querySelector('.card__image').alt = initialCard.name;
+    newCard.querySelector('.card__title').textContent = initialCard.name;
+    newCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return newCard;
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+    const newCard = addCard(card, deleteCard);
+    placesList.append(newCard);
+})
